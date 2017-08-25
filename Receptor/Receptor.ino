7f4c8f5d3f86faeeb2ca3104f7bf53d19f9806c4
Marlon Costa <marlon.costa@ieee.org>
2017-08-25 15:59:10 -0300

    Media adicionada

--- Receptor/Receptor.ino
+++ Receptor/Receptor.ino
@@ -14,6 +14,10 @@ const uint64_t pipe = 0xE13CBAF433LL;
 //Variáveis de armazenamento de tempo
 unsigned long startTime;
 unsigned long elapsedTime;
+unsigned long totalTime;
+unsigned long mean;
+
+unsigned int samples;
 
 void setup()
 {
@@ -29,15 +33,28 @@ void setup()
 
 void loop()
 {
-  //Tempo em que se inicia a captação
-  startTime = micros();
-  if (radio.available()){
-    char text[32] = {0};
-    radio.read(&text, sizeof(text));
-    //Tempo decorrido do início até o recebimento
-    elapsedTime = micros() - startTime;
-    Serial.print(elapsedTime);
-    Serial.println(" µs");
-    Serial.println(text);
+  while (samples < 40){
+    //Tempo em que se inicia a captação
+    startTime = micros();
+    if (radio.available()){
+      char text[32] = {0};
+      radio.read(&text, sizeof(text));
+      //Tempo decorrido do início até o recebimento
+      elapsedTime = micros() - startTime;
+      totalTime = totalTime + elapsedTime;
+      Serial.print(elapsedTime);
+      Serial.println(" µs");
+      Serial.println(text);
+      samples++;
+    }
   }
+  
+  mean = totalTime/samples;
+  Serial.print("Média após ");
+  Serial.print(samples);
+  Serial.print(" amostras: ");
+  Serial.print(mean);
+  Serial.println(" µs");
+  
+  while(1){}
 }
